1928ddaff6bfe137c5c93eb7bf16d6669aeec4c3
Andreas Motl <andreas.motl@panodata.org>
2023-04-07 20:14:45 +0200

    Change license to GPLv3
    When inheriting ideas, inspirations, and code from Kotori, socat and
    GStreamer into core concepts of LorryStream, it will be appropriate to
    adhere to their licensing. Individual components for Streamz will be
    licensed under a BSD 3-Clause license, like Streamz itself.

--- lorrystream/streamz/sinks.py
+++ lorrystream/streamz/sinks.py
@@ -1,6 +1,5 @@
-# Copyright (c) 2013-2023 The Kotori developers and contributors.
-# This module is part of LorryStream and is released under the MIT License.
-# See LICENSE file for more information.
+# Copyright (c) 2013-2023, The Kotori developers and contributors.
+# Distributed under the terms of a BSD 3-Clause license, see LICENSE.
 
 import logging
 import re
