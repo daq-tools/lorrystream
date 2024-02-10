0f0be667a55dc9f9aaebe7562fa96607d1ca75fc
Andreas Motl <andreas.motl@panodata.org>
2024-02-10 15:29:28 +0100

    Testing: Update to cratedb-toolkit 0.0.4, using upstream pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,39 +3,14 @@
 # See LICENSE file for more information.
 
 import pytest
-import sqlalchemy as sa
-from cratedb_toolkit.testing.testcontainers.cratedb import CrateDBContainer
 
 from lorrystream.util.common import setup_logging
 
 
-class CrateDBFixture:
-    def __init__(self):
-        self.cratedb = None
-        self.setup()
-
-    def setup(self):
-        self.cratedb = CrateDBContainer("crate/crate:nightly")
-        self.cratedb.start()
-
-    def finalize(self):
-        self.cratedb.stop()
-
-    def reset(self):
-        database_url = self.cratedb.get_connection_url()
-        sa_engine = sa.create_engine(database_url)
-        with sa_engine.connect() as conn:
-            conn.exec_driver_sql("DROP TABLE IF EXISTS testdrive;")
-
-    def get_connection_url(self, *args, **kwargs):
-        return self.cratedb.get_connection_url(*args, **kwargs)
-
-
-@pytest.fixture(scope="function")
-def cratedb():
-    fixture = CrateDBFixture()
-    yield fixture
-    fixture.finalize()
+@pytest.fixture
+def cratedb(cratedb_service):
+    cratedb_service.reset(["testdrive"])
+    yield cratedb_service
 
 
 setup_logging()
