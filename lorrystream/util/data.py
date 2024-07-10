25950ea6220d5c1e83ba196a64db0f362b34fd6a
Andreas Motl <andreas.motl@panodata.org>
2024-07-10 02:42:24 +0200

    AMQP: Add capability for "reconnect"

--- lorrystream/util/data.py
+++ lorrystream/util/data.py
@@ -63,3 +63,17 @@ def get_sqlalchemy_dialects() -> t.List[str]:
         more = []
 
     return sorted(list(builtins) + list(more))
+
+
+def asbool(obj: t.Any) -> bool:
+    # from sqlalchemy.util.langhelpers
+    # from paste.deploy.converters
+    if isinstance(obj, str):
+        obj = obj.strip().lower()
+        if obj in ["true", "yes", "on", "y", "t", "1"]:
+            return True
+        elif obj in ["false", "no", "off", "n", "f", "0"]:
+            return False
+        else:
+            raise ValueError("String is not true/false: %r" % obj)
+    return bool(obj)
