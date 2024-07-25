a328ef9f2163a8273db21bb1fd6ce725cac0ca0d
Andreas Motl <andreas.motl@panodata.org>
2024-07-25 22:48:41 +0200

    Project: Provide `__appname__` and `__version__` symbols

--- lorrystream/__init__.py
+++ lorrystream/__init__.py
@@ -1,1 +1,10 @@
-from .cmd import parse_launch  # noqa: F401
+from importlib.metadata import version
+
+from .cmd import parse_launch
+
+__appname__ = "lorrystream"
+__version__ = version(__appname__)
+
+__all__ = [
+    "parse_launch",
+]
